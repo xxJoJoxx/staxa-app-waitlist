bc13e616267021288277f3f93efc5d578b97c5a7
xJoJox <jklz521@gmail.com>
2025-04-17 01:02:52 -0500

    more tweaks

--- amplify/functions/email-handler/resource.ts
+++ amplify/functions/email-handler/resource.ts
@@ -1,5 +1,6 @@
 import { defineFunction, secret } from '@aws-amplify/backend';
 
+// Using named export to match the pattern used by auth and data resources
 export const emailHandler = defineFunction({
   name: 'email-handler',
   environment: {
